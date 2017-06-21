52a017f6bf51cc9c1f51f51925549319dcea640d
InnovationBham_1 <InnovationBham_1@UABiLab-1s-iMac.local>
2017-06-21 17:01:10 -0500

    Another commit

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var button = document.createElement("button");
+console.log(button)
 button.innerHTML = "push me";
 var body = document.getElementsByTagName("body")[0];
 button.className = 'button';
 button.style.width = '50px';
 button.style.height = '50px';
 var counter = 0;
+var even = [2,4,6,8,0]
+var odd = [1,3,5,7,9]
 body.appendChild(button);
+
 button.addEventListener ("click", function() {
     var div = document.createElement('div');
     div.id = (counter++);
@@ -24,25 +28,23 @@ button.addEventListener ("click", function() {
     div.style.color = "white";
     div.addEventListener('click', function(){
     div.style.backgroundColor = randomColor();
-    
-    function randomColor() {
-        var randomRed = Math.floor(Math.random() * 350);
-        var randomGreen = Math.floor(Math.random() * 350);
-        var randomBlue = Math.floor(Math.random() * 350);
-        var randomColor = "rgb("+randomRed+","+randomGreen+","+randomBlue+")";
-        return randomColor;
-        };
-});
-    div.addEventListener ("doubleclick", function (){
-        if (counter = 2,4,6,8,0){
-            document.removeChild(div.id[counter+1])
-        } else if (counter =1,3,5,7,9) {
-            document.removeChild(div.id[counter-1])
-        } else {
-            alert.log("element does not exist")
+        function randomColor() {
+            var randomRed = Math.floor(Math.random() * 350);
+            var randomGreen = Math.floor(Math.random() * 350);
+            var randomBlue = Math.floor(Math.random() * 350);
+            var randomColor = "rgb("+randomRed+","+randomGreen+","+randomBlue+")";
+            return randomColor;
         };
     });
     body.appendChild(div);
-
+div.addEventListener ("dblclick", function remove(){
+        if (div.id%2==0){
+            div.nextSibling.remove();    
+        } else if (div.id%2==1){
+            div.previousSibling.remove();
+        } else ;{
+            alert('Element does not exist');
+        }
+        console.log(div.nextElementSibling.remove);
+    });
 });
-
